dbdb557c1ad3f40a65b3f72f62b74dd0b2ff0180
Steve Swanson <steveswanson@Steves-Air.attlocal.net>
2016-07-16 17:51:07 -0500

    -basic error handling/validation added to email field -submit button functionality put into main.js

--- js/main.js
+++ js/main.js
@@ -6,8 +6,12 @@ define(function(require){
   var statesPartial = require('stache!partials/states');
 
   $(function(){
+    // $('#myModal').foundation('reveal', 'open');
     $('.state-dropdown').html( statesPartial() );
 
+    $('a.button.submit').on('click',function(e){
+      $(this).parents('form').submit();      
+    });
 
     $(document).foundation();
   });
