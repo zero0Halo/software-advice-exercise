95e9b430facfad6c655f2a03b14709eb0ac34c09
Steve Swanson <steveswanson@Steves-Air.attlocal.net>
2016-07-16 13:59:05 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# bookmrks
+Google Chrome extension to help me keep better track of my bookmarks
---
+++ js/app.js
@@ -0,0 +1,99 @@
+requirejs.config({
+    baseUrl: "js/lib",
+    paths: {
+      app: "../",
+      views: "../views",
+      fnd: "foundation",
+
+      jquery: "jquery.min",
+      underscore: "underscore.min",
+      backbone: "backbone.min",
+      
+      jqueryCookie: "jquery.cookie",
+      modernizr: "modernizr",
+      placeholder: "placeholder",
+      fastclick: "fastclick",
+
+      foundation: "foundation/foundation",
+      "foundation.abide": "foundation/foundation.abide",
+      "foundation.accordion": "foundation/foundation.accordion",
+      "foundation.alert": "foundation/foundation.alert",
+      "foundation.clearing": "foundation/foundation.clearing",
+      "foundation.dropdown": "foundation/foundation.dropdown",
+      "foundation.equalizer": "foundation/foundation.equalizer",
+      "foundation.interchange": "foundation/foundation.interchange",
+      "foundation.joyride": "foundation/foundation.joyride",      
+      "foundation.magellan": "foundation/foundation.magellan",
+      "foundation.offcanvas": "foundation/foundation.offcanvas",
+      "foundation.orbit": "foundation/foundation.orbit",
+      "foundation.reveal": "foundation/foundation.reveal",
+      "foundation.slider": "foundation/foundation.slider",
+      "foundation.tab": "foundation/foundation.tab",
+      "foundation.tooltip": "foundation/foundation.tooltip",
+      "foundation.topbar": "foundation/foundation.topbar"
+    },
+    shim: {
+    	backbone: {
+    		deps: ["underscore", "jquery"],
+    		exports: "Backbone"
+    	},
+      jqueryCookie: {
+        deps: ["jquery"],
+        exports: "jqueryCookie"
+      },
+      foundation: {
+        deps: ["jquery", "modernizr", "placeholder", "jqueryCookie","fastclick"],
+        exports: "foundation"
+      },
+      "foundation.abide": {
+        deps: ['foundation']
+      },
+      "foundation.accordion": {
+        deps: ['foundation']
+      },
+      "foundation.alert": {
+        deps: ['foundation']
+      },
+      "foundation.clearing": {
+        deps: ['foundation']
+      },
+      "foundation.dropdown": {
+        deps: ['foundation']
+      },
+      "foundation.equalizer": {
+        deps: ['foundation']
+      },
+      "foundation.interchange": {
+        deps: ['foundation']
+      },
+      "foundation.joyride": {
+        deps: ['foundation']
+      },
+      "foundation.magellan": {
+        deps: ['foundation']
+      },
+      "foundation.offcanvas": {
+        deps: ['foundation']
+      },
+      "foundation.orbit": {
+        deps: ['foundation']
+      },
+      "foundation.reveal": {
+        deps: ['foundation']
+      },
+      "foundation.slider": {
+        deps: ['foundation']
+      },
+      "foundation.tab": {
+        deps: ['foundation']
+      },
+      "foundation.tooltip": {
+        deps: ['foundation']
+      },
+      "foundation.topbar": {
+        deps: ['foundation']
+      }
+    }
+});
+
+requirejs(["app/main"]);
