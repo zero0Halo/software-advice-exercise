ee3d6a234882515eadc642dc17050e527193cadc
Steve Swanson <steveswanson@Steves-Air.attlocal.net>
2016-07-19 16:20:57 -0500

    -cleaned up files -added in a lot of comments to AbideForm

--- js/AbideForm.js
+++ js/AbideForm.js
@@ -2,18 +2,25 @@ define(function(require){
   require('jqueryCookie');
   require('underscore');
 
+  // Basic constructor
   var AbideForm = function(obj){
     this.$form = $(obj);
     this.statesPartial = require('stache!partials/states');
     this.imagePaths = ['img/image01.jpg', 'img/image02.jpg'];
     this.imagePath = '';
     this.jsonData = {};
+
+    // I very much like the way events are registered in Backbone.
+    // Very readable and consolidated. 
+    // This object will be parsed in the .registerEvents method.
     this.events = {
-      'valid.fndtn.abide form': 'validForm',
+      'valid.fndtn.abide form': 'validForm', // [event, [object]] : [function]
       'invalid.fndtn.abide form': 'invalidForm',
       'click a.button.custom-submit': 'submitForm'        
     };
   };
+
+  // Adds the state dropdown if the .state-dropdown class exists
   AbideForm.prototype.addStatesDropdown = function(){
     var $obj = this.$form.find('.state-dropdown');
     
@@ -21,7 +28,9 @@ define(function(require){
       $obj.html( this.statesPartial() );
     }
   };
-  AbideForm.prototype.setRandomImage = function(){
+
+  // Gets the path of the random image and applys it.
+  AbideForm.prototype.applyRandomImage = function(){
     var $image = this.$form.find('.cookie-image');
 
     if( !$.cookie('imagePath') ) {
@@ -33,6 +42,8 @@ define(function(require){
       $image.attr('src', $.cookie('imagePath') );
     }
   };
+
+  // Registers events declared in the constructor
   AbideForm.prototype.registerEvents = function(){
     var split, event, $obj, func;
 
@@ -45,6 +56,8 @@ define(function(require){
       $obj.on(event, func);
     }
   };
+
+  // Only fires if the form is valid when the submit button is clicked.
   AbideForm.prototype.validForm = function(){
     var formData = this.$form.serializeArray();
     this._setRandomImage();
@@ -61,19 +74,29 @@ define(function(require){
 
     console.log(this.jsonData);
   };
+
+  // Only fires if the form is invalid when the submit button is clicked.
   AbideForm.prototype.invalidForm = function(){
     console.log('Invalid Form Submission');
   };
+
+  // In all candor, this method is most likely not necessary.
+  // However since I was co-opting the submit method from abide, it seemed prudent to build a method of my own.
+  // Could be useful for later upgrades to the functionality.
   AbideForm.prototype.submitForm = function(){
     this.$form.submit();  
   };
+
+  // Draws a random image path from those set in the constructor and sets it to .imagePath
   AbideForm.prototype._setRandomImage = function(){
     var random = _.random(this.imagePaths.length-1);
     this.imagePath = this.imagePaths[random];
   };  
+
+  // Call this to fire off the prototype.
   AbideForm.prototype.init = function(){
     this.addStatesDropdown();
-    this.setRandomImage();
+    this.applyRandomImage();
     this.registerEvents();
   };  
 
--- js/main.js
+++ js/main.js
@@ -3,18 +3,17 @@ define(function(require){
   require('foundation.reveal');
   require('foundation.abide');
 
+  // Added functionality to a foundation.abide form
   var AbideForm = require('app/AbideForm');
 
   $(function(){
     $(document).foundation();
     
-    
-
     var myForm = new AbideForm('#myModal form');
     myForm.init();
 
-    $('#myModal').foundation('reveal', 'open');
-
+    // Uncomment out the following line to show the form immediately. Handy for styling.
+    // $('#myModal').foundation('reveal', 'open');
   });
 
   
